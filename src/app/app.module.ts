d43b1da0550b27d1b8ffe906af3ce2fbe7a11fb1
DucNguyen <89559446+Nguyenduc2k3@users.noreply.github.com>
2023-05-19 10:59:02 +0700

    Fix code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { ProductlistComponent } from './pages/productlist/productlist.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LayoutAdminComponent } from './components/layouts/layout-admin/layout-admin.component';
+import { LayoutClientComponent } from './components/layouts/layout-client/layout-client.component';
+import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
+import { SidebarAdminComponent } from './components/sidebar-admin/sidebar-admin.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { LayoutComponent } from './layouts/layout/layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,15 @@ import { RegisterComponent } from './pages/register/register.component';
     HeaderComponent,
     ProductlistComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LayoutAdminComponent,
+    LayoutClientComponent,
+    HeaderAdminComponent,
+    SidebarAdminComponent,
+    DashboardComponent,
+    ProductsComponent,
+    LayoutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
